7bc82e91ca375f7e9ca74e8139cccd3f457d7030
sagnik <sagnik.devv@gmail.com>
2025-12-25 12:38:54 +0530

    learned some concepts

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
+from flask import Flask
+
+app= Flask(__name__) # __name__ is a special variable in Python that returns the name of the current module.
+
+@app.route('/') # @app.route is a decorator that is used to bind a function to a URL.
+def home(): # This is the function that will be called when the URL is accessed. 
+    return 'Hello User this is my first flask app' # This is the response that will be sent back to the client. 
+    
 
