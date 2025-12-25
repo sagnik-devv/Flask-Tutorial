86b8578659ea004a756d0e3e5276946531f611da
sagnik <sagnik.devv@gmail.com>
2025-12-25 12:54:02 +0530

    learned a few things on @app.route

--- app.py
+++ app.py
@@ -5,5 +5,10 @@ app= Flask(__name__) # __name__ is a special variable in Python that returns the
 @app.route('/') # @app.route is a decorator that is used to bind a function to a URL.
 def home(): # This is the function that will be called when the URL is accessed. 
     return 'Hello User this is my first flask app' # This is the response that will be sent back to the client. 
-    
+@app.route('/about')
+def about():
+    return 'This is the about page'
+@app.route('/contact')
+def contact():
+    return 'This is the contact page' 
 
