5a16ca6449911bbffbfaa0fa8424487e778df7e6
sagnik <sagnik.devv@gmail.com>
2025-12-25 12:20:28 +0530

    first commit

--- app.py
+++ app.py
@@ -1,17 +1,1 @@
-from flask import Flask,render_template,url_for 
 
-# 1. Initialize the Flask application
-app = Flask(__name__)
-
-# 2. Define a Route
-# This function will be called when someone visits the root URL '/'
-@app.route('/')
-def home():
-    return render_template("index.html")
-
-
-
-# 3. Run the Application
-if __name__ == '__main__':
-    # debug=True allows the server to reload when you change code
-    app.run(debug=True)
